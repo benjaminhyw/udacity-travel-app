cba72054164b2fb5fc4bdc5c3b923103f29effe4
benjaminhyw <benjaminhyw@gmail.com>
2020-06-27 22:28:22 -0700

    Rename index.js to server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,3 @@
-//BEN TODO: This file might need to get renamed to server.js.  Check requirements
-
 const AYLIENTextAPI = require("aylien_textapi");
 const cors = require("cors");
 const dotenv = require("dotenv");
