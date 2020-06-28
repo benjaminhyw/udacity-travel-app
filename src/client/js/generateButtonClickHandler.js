fe96cb42c7f5599012be49be50ccac7bf3a0a51c
benjaminhyw <benjaminhyw@gmail.com>
2020-06-27 22:26:59 -0700

    Update

--- src/client/js/generateButtonClickHandler.js
+++ src/client/js/generateButtonClickHandler.js
@@ -1,3 +1,11 @@
+const WEATHERAPIKEY = "d14498e1d6441199d14053bf26d766d9";
+// const WEATHERAPIKEY = process.env.WEATHERAPIKEY;
+const BASEURL = "https://api.openweathermap.org/data/2.5/weather?";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
 async function generateButtonClickHandler(event) {
   const zip = document.getElementById("zip").value;
   const feelings = document.getElementById("feelings").value;
@@ -20,6 +28,36 @@ async function generateButtonClickHandler(event) {
       let response = await getAllWeatherData("/all");
       console.log(response);
     });
+}
+
+async function fetchWeatherData(zipCode) {
+  const query = `${BASEURL}zip=${zipCode}&appid=${WEATHERAPIKEY}`;
+  return await fetch(query);
+}
+
+async function postWeatherData(url = "", data = {}) {
+  await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+async function getAllWeatherData(route) {
+  const date = document.getElementById("date");
+  const temp = document.getElementById("temp");
+  const content = document.getElementById("content");
+
+  await fetch(route).then(async (result) => {
+    result = await result.json();
+
+    date.innerHTML = result[newDate].date;
+    temp.innerHTML = result[newDate].temperature;
+    content.innerHTML = result[newDate].userResponse;
+  });
 }
 
 export { generateButtonClickHandler };
