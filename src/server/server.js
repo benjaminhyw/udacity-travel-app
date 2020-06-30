3d48c33d28e43bae7e6cdb447f366048d52a95d6
benjaminhyw <benjaminhyw@gmail.com>
2020-06-29 17:35:02 -0700

    Remove unused code in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,3 @@
-const AYLIENTextAPI = require("aylien_textapi");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
@@ -7,11 +6,6 @@ const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 
 dotenv.config();
-
-const textapi = new AYLIENTextAPI({
-  application_id: process.env.API_ID,
-  application_key: process.env.API_KEY,
-});
 
 const app = express();
 app.use(cors());
@@ -34,36 +28,8 @@ app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
 
-app.post("/sentiment", function (req, res) {
-  console.log("::: Running express.post :::");
-  console.log(req.body.text);
-
-  textapi.sentiment({ text: req.body.text }, function (
-    err,
-    result,
-    rateLimits
-  ) {
-    console.log("Error below:");
-    console.log(err);
-    console.log("Result below:");
-    console.log(result);
-    console.log("ERate Limits  below:");
-    console.log(rateLimits);
-    res.send(result);
-  });
-});
-
-const WEATHERAPIKEY = process.env.WEATHERAPIKEY;
-const BASEURL = "https://api.openweathermap.org/data/2.5/weather?";
 const GEONAMESUSERNAME = process.env.GEONAMESUSERNAME;
 const GEONAMESBASEURL = "http://api.geonames.org/searchJSON?q=";
-
-async function fetchWeatherData(zipCode) {
-  const query = `${BASEURL}zip=${zipCode}&appid=${WEATHERAPIKEY}`;
-  const response = await fetch(query);
-
-  return await response.json();
-}
 
 async function fetchWeatherDataGEONAMES(cityName) {
   const query = `${GEONAMESBASEURL}${cityName}&username=${GEONAMESUSERNAME}`;
@@ -83,26 +49,6 @@ function sendData(request, response) {
 }
 
 app.post("/add", geonamesCallBack);
-
-function callBack(request, response) {
-  console.log("POST");
-  fetchWeatherData(request.body.zip)
-    .then((res) => {
-      return res;
-    })
-    .then((res) => {
-      console.log(res);
-      let updatedProjectData = {
-        temperature: res.main.temp,
-        date: request.body.date,
-        userResponse: request.body.feelings,
-      };
-
-      projectData[request.body.date] = updatedProjectData;
-
-      response.send(res);
-    });
-}
 
 function geonamesCallBack(request, response) {
   console.log("POST");
