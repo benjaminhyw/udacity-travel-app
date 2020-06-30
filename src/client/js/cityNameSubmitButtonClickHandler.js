c1a58e97a98cf496a66b50e1f572c5f253088e62
benjaminhyw <benjaminhyw@gmail.com>
2020-06-29 21:38:12 -0700

    Update UI with API results

--- src/client/js/cityNameSubmitButtonClickHandler.js
+++ src/client/js/cityNameSubmitButtonClickHandler.js
@@ -4,6 +4,8 @@ let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 async function cityNameSubmitButtonClickHandler(event) {
   const city = document.getElementById("city").value;
+  const travelDate = document.getElementById("date").value;
+
   event.preventDefault();
 
   await fetch("http://localhost:8081/add", {
@@ -11,27 +13,35 @@ async function cityNameSubmitButtonClickHandler(event) {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ city: city, date: newDate }),
+    body: JSON.stringify({
+      city: city,
+      travelDate: travelDate,
+      todaysDate: newDate,
+    }),
   }).then(async () => {
     await getAllCityData("http://localhost:8081/all");
   });
 }
 
 async function getAllCityData(route) {
-  const date = document.getElementById("date");
+  const todaysDate = document.getElementById("todaysDate");
   const cityName = document.getElementById("cityName");
   const latitude = document.getElementById("latitude");
   const longitude = document.getElementById("longitude");
   const country = document.getElementById("country");
+  const travelDate = document.getElementById("travelDate");
 
   await fetch(route).then(async (result) => {
     result = await result.json();
 
-    date.innerHTML = result[newDate].date;
-    cityName.innerHTML = result[newDate].cityName;
-    latitude.innerHTML = result[newDate].latitude;
-    longitude.innerHTML = result[newDate].longitude;
-    country.innerHTML = result[newDate].country;
+    // BEN TODO: if you use newDate as the way you enter data, it will always get overwritten
+    // You will have to come back to this.
+    todaysDate.innerHTML = `Today's Date: ${result[newDate].todaysDate}`;
+    cityName.innerHTML = `Target Destination: ${result[newDate].cityName}`;
+    latitude.innerHTML = `Latitude: ${result[newDate].latitude}`;
+    longitude.innerHTML = `Longitude: ${result[newDate].longitude}`;
+    country.innerHTML = `Country: ${result[newDate].country}`;
+    travelDate.innerHTML = `Travel Date: ${result[newDate].travelDate}`;
   });
 }
 
