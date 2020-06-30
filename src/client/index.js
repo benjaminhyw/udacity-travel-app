ed8acd5e9be6bc6a63301f7a528a165929d07f1c
benjaminhyw <benjaminhyw@gmail.com>
2020-06-29 17:42:32 -0700

    Remove unused code in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,20 +1,10 @@
 import { handleSubmit } from "./js/formHandler";
 import { isValueEmpty } from "./js/isValueEmpty";
-import { generateButtonClickHandler } from "./js/generateButtonClickHandler";
 import { cityNameSubmitButtonClickHandler } from "./js/cityNameSubmitButtonClickHandler";
 
 import "./styles/style.scss";
 
-// I think this is where you add the buttton handler
-const generateButton = document.getElementById("generate");
-generateButton.addEventListener("click", generateButtonClickHandler);
-
 const handleSubmitButton = document.getElementById("handleSubmit");
 handleSubmitButton.addEventListener("click", cityNameSubmitButtonClickHandler);
 
-export {
-  handleSubmit,
-  isValueEmpty,
-  generateButtonClickHandler,
-  cityNameSubmitButtonClickHandler,
-};
+export { handleSubmit, isValueEmpty, cityNameSubmitButtonClickHandler };
