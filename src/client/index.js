a21c48a943e777c6e9869680f300c1b214397dca
benjaminhyw <benjaminhyw@gmail.com>
2020-06-30 22:52:13 -0700

    Delete formHandler file, remove references

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,3 @@
-import { handleSubmit } from "./js/formHandler";
 import { isValueEmpty } from "./js/isValueEmpty";
 import { submitHandler } from "./js/submitHandler";
 
@@ -7,4 +6,4 @@ import "./styles/style.scss";
 const handleSubmitButton = document.getElementById("handleSubmit");
 handleSubmitButton.addEventListener("click", submitHandler);
 
-export { handleSubmit, isValueEmpty, submitHandler };
+export { isValueEmpty, submitHandler };
