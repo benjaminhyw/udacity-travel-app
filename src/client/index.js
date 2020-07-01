d9a2c300ea9d6560af60e1d6f7ede009c4519b09
benjaminhyw <benjaminhyw@gmail.com>
2020-06-30 22:35:51 -0700

    Rename cityNameSubmitButtonClickHandler to submitHandler

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,10 @@
 import { handleSubmit } from "./js/formHandler";
 import { isValueEmpty } from "./js/isValueEmpty";
-import { cityNameSubmitButtonClickHandler } from "./js/cityNameSubmitButtonClickHandler";
+import { submitHandler } from "./js/submitHandler";
 
 import "./styles/style.scss";
 
 const handleSubmitButton = document.getElementById("handleSubmit");
-handleSubmitButton.addEventListener("click", cityNameSubmitButtonClickHandler);
+handleSubmitButton.addEventListener("click", submitHandler);
 
-export { handleSubmit, isValueEmpty, cityNameSubmitButtonClickHandler };
+export { handleSubmit, isValueEmpty, submitHandler };
