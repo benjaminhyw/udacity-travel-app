f35eff75d55bec2f5be5d9167c9642584c429d0f
benjaminhyw <benjaminhyw@gmail.com>
2020-07-02 14:48:13 -0700

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,9 @@ import { submitHandler } from "./js/submitHandler";
 import "./styles/style.scss";
 import "./styles/responsive.scss";
 
-const submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", submitHandler);
+document.addEventListener("DOMContentLoaded", () => {
+  const submitButton = document.getElementById("submit");
+  submitButton.addEventListener("click", submitHandler);
+});
 
 export { isValueEmpty, submitHandler };
