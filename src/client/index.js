48bc014cfbccd2a79fa90932a56194ed9049f749
benjaminhyw <benjaminhyw@gmail.com>
2020-07-01 22:30:26 -0700

    Reference new responsive sheet

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { isValueEmpty } from "./js/isValueEmpty";
 import { submitHandler } from "./js/submitHandler";
 
 import "./styles/style.scss";
+import "./styles/responsive.scss";
 
 const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", submitHandler);
